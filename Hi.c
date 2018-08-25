03ef3fd41ebcc7abca0328efa22fe2bd1f314d60
Linhan Li <lmax.qaz@gmail.com>
2018-08-24 19:34:58 -0500

    Update Hi.c

--- Hi.c
+++ Hi.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char const *argv[]) {
   printf("Li Linhan  Computer Science\n");
-  printf("Jia Jiahua Computer Science\n")
-  printf("Wendian Zhang Computer Science\n")
+  printf("Jia Jiahua Computer Science\n");
+  printf("Wendian Zhang Computer Science\n");
     return 0;
 }
