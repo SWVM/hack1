a46e56a38bb57342c8e132e50fc5a3799a99ea47
Li Linhan <lmax.qaz@gmail.com>
2018-08-28 07:01:57 -0700

    guud

---
+++ dollar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define JPY_PER_USD  76.80
+#define POUND_PER_USD  0.6
+
+int main(int argc, char const *argv[]) {
+
+  printf("Please input the total amount of US Dollars:\n");
+
+  double USD = 0;
+  scanf("%f\n", &USD);
+  USD *= 0.9;
+
+  double JPY = (USD/2) * JPY_PER_USD;
+  double POUND = (USD/2) * POUND_PER_USD;
+  printf("You get %f British Pounds and %f Japanese JPY.\n", JPY, POUND);
+
+    return 0;
+}
