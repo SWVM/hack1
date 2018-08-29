63e6f16336ffe18b3057c5017626ba2c66660024
Li Linhan <lmax.qaz@gmail.com>
2018-08-29 15:21:14 -0700

    kk

--- Hi.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char const *argv[]) {
-  printf("Li Linhan  Computer Science\n");
-  printf("Jia Jiahua Computer Science\n");
-  printf("Wendian Zhang Computer Science\n");
-    return 0;
-}
